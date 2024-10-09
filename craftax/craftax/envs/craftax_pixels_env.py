466769a2c0e8759ecce71ff40b74e1ac4062f988
I-AM-PROTO <kty2396@gmail.com>
2024-10-08 22:58:12 -0700

    Fix observation space shape in pixel env

--- craftax/craftax/envs/craftax_pixels_env.py
+++ craftax/craftax/envs/craftax_pixels_env.py
@@ -76,8 +76,8 @@ class CraftaxPixelsEnvNoAutoReset(EnvironmentNoAutoReset):
             0.0,
             1.0,
             (
-                OBS_DIM[1] * BLOCK_PIXEL_SIZE_AGENT,
                 (OBS_DIM[0] + INVENTORY_OBS_HEIGHT) * BLOCK_PIXEL_SIZE_AGENT,
+                OBS_DIM[1] * BLOCK_PIXEL_SIZE_AGENT,
                 3,
             ),
             dtype=jnp.float32,
